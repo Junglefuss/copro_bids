d14711e70acead8dacebc74726fe77593575c541
Jonathan Goldfuss <jgoldfuss@coproductions.com>
2019-11-07 11:25:05 -0500

    updated views, created URLs, base html

--- copro_bids/urls.py
+++ copro_bids/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='project_list'),
+    path('projects/new', views.project_create, name='project_create'),
+    path('projects/<int:pk>/edit', views.project_edit, name='project_edit'),
+
+    path('teammate/new', views.teammate_create, name='teammate_create'),
+    path('teammate/<int:pk>', views.teammate_detail, name='teammate_detail'),
+    path('teammate/<int:pk>/edit', views.teammate_edit, name='teammate_edit'),
+    path('teammate/<int:pk>/delete', views.teammate_delete, name='teammate_delete'),
+
+    path('bid/new', views.bid_create, name='bid_create'),
+    path('bid/<int:pk>', views.bid_detail, name='bid_detail'),
+    path('bid/<int:pk>/edit', views.bid_edit, name='bid_edit'),
 ]
--- copro_bids/views.py
+++ copro_bids/views.py
@@ -6,3 +6,81 @@ from .models import Project, Bid, Teammate
 def project_list(request):
     projects = Project.objects.all()
     return render(request, 'copro_bids/project_list.html', {'projects': projects})
+
+
+def project_create(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save()
+            return redirect('project_detail', pk=project.pk)
+    else:
+        form = ProjectForm()
+    return render(request, 'copro_bids/project_form.html', {'form': form})
+
+
+def project_edit(request, pk):
+    project = Project.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            project = form.save()
+            return redirect('project_detail', pk=project.pk)
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'copro_bids/project_form.html', {'form': form})
+
+
+def teammate_create(request):
+    if request.method == 'POST':
+        form = TeammateForm(request.POST)
+        if form.is_valid():
+            teammate = form.save()
+            return redirect('teammate_detail', pk=teammate.pk)
+
+
+def teammate_detail(request, pk):
+    teammate = Teammate.objects.get(id=pk)
+    return render(request, 'copro_bids/teammate_detail.html', {'teammate': teammate})
+
+
+def teammate_edit(request, pk):
+    teammate = Teammate.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = TeammateForm(request.POST, instance=teammate)
+        if form.is_valid():
+            teammate = form.save()
+            return redirect('teammate_detail', pk=teammate.pk)
+    else:
+        form = TeammateForm(instance=teammate)
+    return render(request, 'copro_bids/teammate_form.html', {'form': form})
+
+
+def teammate_delete(request, pk):
+    teammate = Teammate.objects.get(id=pk)
+    return redirect('teammate_list')
+
+
+def bid_create(request):
+    if request.method == 'POST':
+        form = BidForm(request.POST)
+        if form.is_valid():
+            bid = form.save()
+            return redirect('bid_detail', pk=song.pk)
+
+
+def bid_detail(request, pk):
+    bid = Bid.objects.get(id=pk)
+    return render(request, 'copro_bids/bid_detail.html', {'bid': bid})
+
+
+def bid_edit(request, pk):
+    bid = Bid.objects.get(pk=pk)
+    if request.method == 'POST':
+        bid = BidForm(request.POST, instance=bid)
+        if form.is_valid():
+            project = form.save()
+            return redirect('bid_detail', pk=song.pk)
+    else:
+        form = BidForm(instance=bid)
+    return render(request, 'copro_bids/bid_form.html', {'form': form})
